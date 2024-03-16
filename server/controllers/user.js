09400bc5b227a57af000dea4f1428cf193c16f12
Nguyet <lethinguyet13112002@gmail.com>
2024-03-16 15:04:02 +0700

    add accesstoken and verify accesstoken

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
+const { generateAccessToken, generateRefreshToken } = require('../middlewares/jwt')
 
 const register = asyncHandler(async (req, res) => {
     const { email, password, firstname, lastname } = req.body
@@ -29,15 +30,33 @@ const login = asyncHandler(async (req, res) => {
         })
     const response = await User.findOne({ email: email })
     if (response && await response.isCorrectPassword(password)) {
-        const { password, ...userData } = response.toObject()
+        const { password, role, ...userData } = response.toObject()
+        //Tạo access token
+        const accessToken = generateAccessToken(response._id, role)
+        const refreshToken = generateRefreshToken(response._id)
+        // Lưu refresh token vào db
+        await User.findByIdAndUpdate(response._id, { refreshToken: refreshToken }, { new: true })
+        //Lưu refresh token vào cookie
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 60 * 60 * 24 * 5 * 1000 })
         return res.status(200).json({
             success: response ? true : false,
+            accessToken,
             userData
         })
     } else throw new Error('Email or password is incorrect')
 })
 
+const getCurrent = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const user = await User.findById(_id).select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: false,
+        rs: user ? user : 'User not found'
+    })
+})
+
 module.exports = {
     register,
-    login
+    login,
+    getCurrent
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const control = require('../controllers/user')
+const { verifyAccessToken } = require('../middlewares/verifyToken')
 
 router.post('/register', control.register)
 router.post('/login', control.login)
+router.get('/current', verifyAccessToken, control.getCurrent)
+
 
 module.exports = router
