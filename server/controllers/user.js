456b0de0877c43a4aaf946d6b8f375a512649910
Nguyet <lethinguyet13112002@gmail.com>
2024-03-19 22:52:07 +0700

    refresh token and logout

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
 const { generateAccessToken, generateRefreshToken } = require('../middlewares/jwt')
+const jwt = require('jsonwebtoken')
 
 const register = asyncHandler(async (req, res) => {
     const { email, password, firstname, lastname } = req.body
@@ -55,8 +56,40 @@ const getCurrent = asyncHandler(async (req, res) => {
     })
 })
 
+// Tạo lại access token sau khi hết hạn
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    //Lấy token từ cookie
+    const cookie = req.cookies
+    if (!cookie && !cookie.refreshToken) throw new Error('No refresh token in cookie')
+    //Check token co hop le hay khong
+    const rs = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET)
+    const response = await User.findOne({ _id: rs._id, refreshToken: cookie.refreshToken })
+    return res.status(200).json({
+        success: response ? true : false,
+        newAccessToken: response ? generateAccessToken(response._id, response.role) : 'Refresh token not matched'
+    })
+})
+
+const logout = asyncHandler(async (req, res) => {
+    const cookie = req.cookies
+    if (!cookie || !cookie.refreshToken) throw new Error('No refresh token in cookies')
+    //xoa refresh token o db
+    await User.findOneAndUpdate({ refreshToken: cookie.refreshToken }, { refreshToken: '' }, { new: true })
+    //xoa refresh token o cookie
+    res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: true
+    })
+    return res.status(200).json({
+        success: true,
+        mes: 'Logout is successful'
+    })
+})
+
 module.exports = {
     register,
     login,
-    getCurrent
+    getCurrent,
+    refreshAccessToken,
+    logout
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ const { verifyAccessToken } = require('../middlewares/verifyToken')
 router.post('/register', control.register)
 router.post('/login', control.login)
 router.get('/current', verifyAccessToken, control.getCurrent)
-
+router.post('/refreshtoken', control.refreshAccessToken)
+router.get('/logout', control.logout)
 
 module.exports = router
