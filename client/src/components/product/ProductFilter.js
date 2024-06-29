86074ef030ed549e8cc0bf0b9477ef4b7a3227a7
dinhminhx <nghiemdinhminh2002@gmail.com>
2024-06-29 14:30:02 +0700

    filter function

--- client/src/components/product/ProductFilter.js
+++ client/src/components/product/ProductFilter.js
@@ -1,32 +1,42 @@
-import React, { memo } from 'react'
-import ProductFilterSelection from './ProductFilterSelection'
-import { productFilter } from '../../ultils/contants'
+import React, { memo } from "react";
+import ProductFilterSelection from "./ProductFilterSelection";
+import { productFilter } from "../../ultils/contants";
 
-const ProductFilter = () => {
+const ProductFilter = ({ selected, setSelected }) => {
     return (
-        <div className=' w-full flex flex-col sm:grid grid-cols-2 gap-2'>
+        <div className=" w-full flex flex-col sm:grid grid-cols-2 gap-2">
             <ProductFilterSelection
-                label='Khoảng giá'
+                label="Khoảng giá"
                 items={productFilter.price}
+                selected={selected}
+                setSelected={setSelected}
             />
             <ProductFilterSelection
-                label='Thương hiệu'
+                label="Thương hiệu"
                 items={productFilter.brand}
+                selected={selected}
+                setSelected={setSelected}
             />
             <ProductFilterSelection
-                label='Loại trang phục'
+                label="Loại trang phục"
                 items={productFilter.category}
+                selected={selected}
+                setSelected={setSelected}
             />
             <ProductFilterSelection
-                label='Màu sắc'
+                label="Màu sắc"
                 items={productFilter.color}
+                selected={selected}
+                setSelected={setSelected}
             />
             <ProductFilterSelection
-                label='Phong cách'
+                label="Phong cách"
                 items={productFilter.style}
+                selected={selected}
+                setSelected={setSelected}
             />
         </div>
-    )
-}
+    );
+};
 
-export default memo(ProductFilter)
+export default memo(ProductFilter);
